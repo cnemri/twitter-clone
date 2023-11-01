0af3a75a6112d51071537b0ac97b1a5cd78ae7fa
cnemri <chouaib.nemri@gmail.com>
2023-11-01 20:01:48 +0100

    Fixed some UI stuff

--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -19,8 +19,10 @@ const FollowBar = (props: Props) => {
   return (
     <div className="px-6 py-4 hidden lg:block">
       <div className="bg-neutral-800 rounded-xl p-4">
-        <h2 className="text-white text-xl font-semibold">Who to follow?</h2>
-        <div className="flex flex-col gap-6 mt-4">
+        <h2 className="text-white text-xl text-center font-semibold border-b border-neutral-600">
+          Who to follow?
+        </h2>
+        <div className="flex flex-col gap-6 mt-4 max-h-96 overflow-scroll">
           {users.map((user: Record<string, any>, index: number) => (
             <div key={index} className="flex flex-row gap-4">
               <Avatar userId={user.id} />
